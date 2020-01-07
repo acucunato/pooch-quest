29b44974fb91bdb6b8cc3c01ccb60f9bfe3a14bd
jsmithxyz <56512278+jsmithxyz@users.noreply.github.com>
2020-01-07 18:56:53 -0500

    Dogapi calls (#11)
    * made response calls
    
    * put in placeholder event listener for search button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
-$(document).ready(function() {
-  function adoptAPI(breed, location, status) {
+$(document).ready(function () {
+
+  // Function pull nearest adoptable dogs
+  function adoptAPI(breed, location, sex) {
     var CLIENT_ID = "VCczzgBh8Mbtp61y6gfIIUFuFKPvGPo2Rt2tIwfskw1UOfylEg";
     var CLIENT_SECRET = "zudVxDG4Hcl82bK8u85yFqcIpQndRKRH8ARqYnvI";
     fetch("https://api.petfinder.com/v2/oauth2/token", {
@@ -14,34 +16,33 @@ $(document).ready(function() {
       },
       method: "POST"
     })
-      .then(function(response) {
+      .then(function (response) {
         return response.json();
       })
-      .then(function(response) {
+      .then(function (response) {
         fetch(
           "https://api.petfinder.com/v2/animals?type=Dog&distance=50&status=adoptable&breed=" +
-            breed +
-            "&location=" +
-            location,
+          breed +
+          "&location=" +
+          location +
+          "&sex=" +
+          sex,
           {
             headers: {
               Authorization: "Bearer " + response.access_token
             }
           }
         )
-          .then(function(response) {
+          .then(function (response) {
             return response.json();
           })
-          .then(function(response) {
+          .then(function (response) {
             console.log(response);
           });
       });
   }
-  adoptAPI("Yorkshire Terrier", 19125);
 
-
-
-
+  // Function to pull Breed Information for API database
   function dogAPI(breed) {
     var API_KEY = "1337f97b-fce8-4485-adca-4714c90a8c1a";
     var urlquery =
@@ -52,12 +53,54 @@ $(document).ready(function() {
     $.ajax({
       url: urlquery,
       method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
       console.log(response);
+
+      var dogName = $("<p>");
+      dogName.text(response[0].name);
+      console.log(response[0].name);
+
+      var dogWeight = $("<p>");
+      dogWeight.text(response[0].weight.imperial);
+      console.log(response[0].weight.imperial);
+
+      var dogHeight = $("<p>");
+      dogHeight.text(response[0].height.imperial);
+      console.log(response[0].height.imperial);
+
+      var lifeSpan = $("<p>");
+      lifeSpan.text(response[0].life_span);
+      console.log(response[0].life_span);
+
+      var dogTemperament = $("<p>");
+      dogTemperament.text(response[0].temperament);
+      console.log(response[0].temperament);
+
+      var breedGroup = $("<p>");
+      breedGroup.text(response[0].breed_group);
+      console.log(response[0].breed_group);
+
+      var bredFor = $("<p>");
+      bredFor.text(response[0].bred_for);
+      console.log(response[0].bred_for);
+
+
     });
   }
-  dogAPI("Yorkshire Terrier");
 
+  // Event Listener for Search Button
+  $("#searchBtn").on("click", function (event) {
+    event.preventDefault();
+    $(".conditions").empty();
+    var breed = $("#breed_name").val().trim();
+    var location = $("#location_name").val().trim();
+    var sex = $("#sex").val().trim();
+
+    dogAPI(breed);
+    adoptAPI(breed, location, sex);
+  })
+
+  // JQuery for Sidenav functionality
   $('.sidenav').sidenav();
 });
 
