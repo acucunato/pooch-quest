e586217096d0597bd0fdb6e651070081b40bff1a
Alyssa Cucunato <56642085+acucunato@users.noreply.github.com>
2020-01-07 21:14:21 -0500

    styling (#16)
    Co-authored-by: jsmithxyz <56512278+jsmithxyz@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-
-
-$(document).ready(function () {
-
-
+$(document).ready(function() {
   // Function pull nearest adoptable dogs
   function adoptAPI(breed, location, sex) {
-
     var CLIENT_ID = "VCczzgBh8Mbtp61y6gfIIUFuFKPvGPo2Rt2tIwfskw1UOfylEg";
     var CLIENT_SECRET = "zudVxDG4Hcl82bK8u85yFqcIpQndRKRH8ARqYnvI";
     fetch("https://api.petfinder.com/v2/oauth2/token", {
@@ -19,18 +14,17 @@ $(document).ready(function () {
       },
       method: "POST"
     })
-
-      .then(function (response) {
+      .then(function(response) {
         return response.json();
       })
-      .then(function (response) {
+      .then(function(response) {
         fetch(
           "https://api.petfinder.com/v2/animals?type=Dog&distance=50&status=adoptable&breed=" +
-          breed +
-          "&location=" +
-          location +
-          "&sex=" +
-          sex,
+            breed +
+            "&location=" +
+            location +
+            "&sex=" +
+            sex,
 
           {
             headers: {
@@ -38,44 +32,61 @@ $(document).ready(function () {
             }
           }
         )
-          .then(function (response) {
+          .then(function(response) {
             return response.json();
           })
-          .then(function (response) {
-
+          .then(function(response) {
             console.log(response);
- 
-    for (i = 0; i < response.animals.length; i++) {
-      var name = response.animals[i].name;
-      var age = response.animals[i].age;
-      var description = response.animals[i].description;
-      var urlLink = response.animals[i].url;
-      var distance = response.animals[i].distance;
-      var photo = response.animals[i].photos[0].small;
 
-      var newCard = $('<div class="col s12 m7">').html('<h2 class="header">' + name + '   ' + age + '</h2>' +
-      '<div class="card horizontal">' +
-       '<div class="card-image">' +
-        '<img src="' + photo + '">' +
-        '</div>' +
-        '<div class="card-stacked">' +
-        '<div class="card-content">' +
-         '<p>' + description + '</p>' +
-         '<p>' + distance + '</p>' +
-        '</div>' +
-        '<div class="card-action">' +
-        '<a href="' + urlLink + '"> Your Quest is Over - Click Here to Adopt Me! </a>' +
-        '</div>' + 
-        '</div>' +
-      '</div>' +
-    '</div>')
-    }
+            for (i = 0; i < response.animals.length; i++) {
+              var name = response.animals[i].name;
+              var age = response.animals[i].age;
+              var description = response.animals[i].description;
+              var urlLink = response.animals[i].url;
+              var distance = response.animals[i].distance;
+              var photo = response.animals[i].photos[0].medium;
 
-    $("#adopt-section").append(newCard)
-  });
-});
-}
+              var newCard = $('<div class="col s12 m7">').html(
+                '<h3 class="header">' +
+                  name +
+                  "</h3>" +
+                  '<div class="card horizontal">' +
+                  '<div class="card-image">' +
+                  '<img src="' +
+                  photo +
+                  '">' +
+                  "</div>" +
+                  '<div class="card-stacked">' +
+                  '<div class="card-content">' +
+                  "<p>" +
+                  "A few tidbits about this pooch: " +
+                  description +
+                  "</p>" +
+                  "<p>" +
+                  "Age: " +
+                  age +
+                  "</p>" +
+                  "<p>" +
+                  "This pooch could be yours with a small journey of " +
+                  distance.toFixed(1) +
+                  " miles." +
+                  "</p>" +
+                  "</div>" +
+                  '<div class="card-action">' +
+                  '<a href="' +
+                  urlLink +
+                  '" target="_blank"> Your Quest is Over - Click Here to Adopt Me! </a>' +
+                  "</div>" +
+                  "</div>" +
+                  "</div>" +
+                  "</div>"
+              );
 
+              $("#adopt-section").append(newCard);
+            }
+          });
+      });
+  }
 
   // Function to pull Breed Information for API database
   function dogAPI(breed) {
@@ -88,23 +99,23 @@ $(document).ready(function () {
     $.ajax({
       url: urlquery,
       method: "GET"
-    }).then(function (response) {
+    }).then(function(response) {
       console.log(response);
 
       var infoCard = $("<div>");
 
       var dogName = $("<p>");
-      dogName.text("Name: "+ response[0].name);
+      dogName.text("Name: " + response[0].name);
       dogName.appendTo(infoCard);
       console.log(response[0].name);
 
       var dogWeight = $("<p>");
-      dogWeight.text("Weight: " + response[0].weight.imperial+ " lbs");
+      dogWeight.text("Weight: " + response[0].weight.imperial + " lbs");
       dogWeight.appendTo(infoCard);
       console.log(response[0].weight.imperial);
 
       var dogHeight = $("<p>");
-      dogHeight.text("Height: " + response[0].height.imperial+" inches");
+      dogHeight.text("Height: " + response[0].height.imperial + " inches");
       dogHeight.appendTo(infoCard);
       console.log(response[0].height.imperial);
 
@@ -114,12 +125,12 @@ $(document).ready(function () {
       console.log(response[0].life_span);
 
       var dogTemperament = $("<p>");
-      dogTemperament.text("Temperament: "+ response[0].temperament);
+      dogTemperament.text("Temperament: " + response[0].temperament);
       dogTemperament.appendTo(infoCard);
       console.log(response[0].temperament);
 
       var breedGroup = $("<p>");
-      breedGroup.text("Breed Group: "+ response[0].breed_group);
+      breedGroup.text("Breed Group: " + response[0].breed_group);
       breedGroup.appendTo(infoCard);
       console.log(response[0].breed_group);
 
@@ -129,15 +140,11 @@ $(document).ready(function () {
       console.log(response[0].bred_for);
 
       infoCard.appendTo("#breedSection");
-
-
-
-
     });
   }
 
   // Event Listener for Search Button
-  $("#searchBtn").on("click", function (event) {
+  $("#searchBtn").on("click", function(event) {
     event.preventDefault();
 
     $("#breedSection").empty();
@@ -148,10 +155,8 @@ $(document).ready(function () {
 
     dogAPI(breed);
     adoptAPI(breed, location, sex);
-  })
+  });
 
   // JQuery for Sidenav functionality
-  $('.sidenav').sidenav();
+  $(".sidenav").sidenav();
 });
-
-
